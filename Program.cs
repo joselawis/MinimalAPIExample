2609b0b6954221903fe1c25d6ed436eff68dd87e
joseluiscontreras <joseluis.contreras@innocv.com>
2024-02-02 13:10:26 +0100

    extension method for mapGroup

--- Program.cs
+++ Program.cs
@@ -1,50 +1,8 @@
-using System.Text.Json;
-using MinimalAPI.Models;
+using MinimalAPI.RouteGroups;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var products = new List<Product>()
-{
-    new() { Id = 1, ProductName = "Smartphone" },
-    new() { Id = 2, ProductName = "SmartTv" }
-};
-
-var productsMapGroup = app.MapGroup("/products");
-
-// GET /products
-productsMapGroup.MapGet(
-    "/",
-    async (HttpContext context) =>
-    {
-        await context.Response.WriteAsync(JsonSerializer.Serialize(products));
-    }
-);
-
-// GET /products/{Id}
-productsMapGroup.MapGet(
-    "/{id:int}",
-    async (HttpContext context, int id) =>
-    {
-        var product = products.Where(temp => temp.Id == id).FirstOrDefault();
-        if (product == null)
-        {
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            await context.Response.WriteAsync("Incorrect Product Id");
-            return;
-        }
-        await context.Response.WriteAsync(JsonSerializer.Serialize(product));
-    }
-);
-
-// POST /products
-productsMapGroup.MapPost(
-    "/",
-    async (HttpContext context, Product product) =>
-    {
-        products.Add(product);
-        await context.Response.WriteAsync("Product added");
-    }
-);
+app.MapGroup("/products").ProductsAPI();
 
 app.Run();
---
+++ RouteGroups/ProductsMapGroup.cs
@@ -0,0 +1,54 @@
+using System.Text.Json;
+using MinimalAPI.Models;
+
+namespace MinimalAPI.RouteGroups
+{
+    public static class ProductsMapGroup
+    {
+        static readonly List<Product> products =
+            new()
+            {
+                new() { Id = 1, ProductName = "Smartphone" },
+                new() { Id = 2, ProductName = "SmartTv" }
+            };
+
+        public static RouteGroupBuilder ProductsAPI(this RouteGroupBuilder group)
+        {
+            // GET /products
+            group.MapGet(
+                "/",
+                async (HttpContext context) =>
+                {
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(products));
+                }
+            );
+
+            // GET /products/{Id}
+            group.MapGet(
+                "/{id:int}",
+                async (HttpContext context, int id) =>
+                {
+                    var product = products.Where(temp => temp.Id == id).FirstOrDefault();
+                    if (product == null)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                        await context.Response.WriteAsync("Incorrect Product Id");
+                        return;
+                    }
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(product));
+                }
+            );
+
+            // POST /products
+            group.MapPost(
+                "/",
+                async (HttpContext context, Product product) =>
+                {
+                    products.Add(product);
+                    await context.Response.WriteAsync("Product added");
+                }
+            );
+            return group;
+        }
+    }
+}
